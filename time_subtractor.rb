bb18cbd0f37319b38eaa652c24d97a5021a6f94d
sweetyclem <pirlot.clementine@gmail.com>
2018-08-18 15:07:31 +0200

    make subtract range work for first few cases

--- time_subtractor.rb
+++ time_subtractor.rb
@@ -10,20 +10,34 @@ end
 
 def included_range first, second
   included = nil
-  # return included if (first.end_time < second.start_time or second.end_time < first.start_time)
-  if first.start_time <= second.start_time
-    if first.end_time >= second.end_time
-      included = second
-    end
+  if first.start_time <= second.start_time && first.end_time >= second.end_time
+    included = second
+  elsif second.start_time > first.start_time
+    included = TimeRange.new(nil, nil)
+    included.start_time = second.start_time
+    included.end_time = first.end_time <= second.end_time ? first.end_time : second.end_time
   end
   return included
 end
 
+# (9, 10) (9, 9.5)
+# included 9-9.5
+# -> 9.5-10
+# 9-10 / 9-9.5
 def subtract_range first, to_subtract
   subtracted = TimeRange.new(nil, nil)
-  subtracted.start_time = to_subtract.end_time >= first.start_time ? to_subtract.end_time : first.start_time
-  subtracted.end_time = first.end_time
-  return nil if subtracted.start_time == subtracted.end_time
+  if first.start_time < to_subtract.start_time
+    subtracted.start_time = first.start_time
+  elsif first.start_time >= to_subtract.start_time
+    subtracted.start_time = to_subtract.end_time
+  end
+  if to_subtract.start_time > first.end_time
+    subtracted.end_time = to_subtract.start_time
+  elsif to_subtract.start_time <= first.end_time
+    subtracted.end_time = first.end_time
+  end
+  return nil if subtracted.start_time >= subtracted.end_time
+  # puts "subtracted: #{subtracted.start_time}-#{subtracted.end_time}"
   return subtracted
 end
 
@@ -45,25 +59,17 @@ end
  
 array = remove_overlap([TimeRange.new(9, 10)], [TimeRange.new(9, 9.5)])
 puts array.inspect
+puts "false" if array.length == 0 # || array[0].start_time != 9.5 || array[0].end_time != 10
 array = remove_overlap([TimeRange.new(9, 10)], [TimeRange.new(9, 10)])
 puts array.inspect
+puts "false" if array.length != 0
 array = remove_overlap([TimeRange.new(9, 9.5)], [TimeRange.new(9.5, 15)])
 puts array.inspect
-array = remove_overlap([TimeRange.new(9, 9.5), TimeRange.new(10, 10.5)], [TimeRange.new(9.25, 10.25)])
+puts "false" if !array || array[0].start_time != 9 || array[0].end_time != 9.5
+array = remove_overlap([TimeRange.new(9, 9.5), TimeRange.new(10, 10.5)], [TimeRange.new(9.25, 10.25)]) # included 9.25-9.5
 puts array.inspect
+puts "false" if !array || array[0].start_time != 9 || array[0].end_time != 9.25 || array[1].start_time != 10.25 || array[1].end_time != 10.5
 
-# puts Time.parse("15:30")
-# to_subtract = included_range(TimeRange.new(9, 12), TimeRange.new(10, 11)) # -> 9-10, 11-12
-# puts "#{to_subtract.start_time}-#{to_subtract.end_time}"
-# to_subtract = included_range(TimeRange.new(9, 12), TimeRange.new(10, 12))
-# puts "#{to_subtract.start_time}-#{to_subtract.end_time}"
-# to_subtract = included_range(TimeRange.new(9, 12), TimeRange.new(8, 8.5))
-# puts "#{to_subtract.start_time}-#{to_subtract.end_time}"
-
-# parcourir liste 1 
-#   parcourir liste 2
-# transformer en secondes
-# comparer chaque time de liste 2 avec chaque de liste 1
 # trier les listes
 # enlever les doublons au début
 # (9:00-10:00) “minus” (9:00-9:30) = (9:30-10:00)
