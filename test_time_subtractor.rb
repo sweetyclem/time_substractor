89155e7d9063570b53e3af0d2bac6c472cc823c5
sweetyclem <pirlot.clementine@gmail.com>
2018-08-20 12:02:11 +0200

    add all instructions test cases

--- test_time_subtractor.rb
+++ test_time_subtractor.rb
@@ -2,12 +2,6 @@ require 'minitest/autorun'
 require_relative './time_range.rb'
 require_relative './time_subtractor.rb'
 
-# (9:00-10:00) “minus” (9:00-9:30) = (9:30-10:00)
-# (9:00-10:00) “minus” (9:00-10:00) = ()
-# (9:00-9:30) “minus” (9:30-15:00) = (9:00-9:30)
-# (9:00-9:30, 10:00-10:30) “minus” (9:15-10:15) = (9:00-9:15, 10:15-10:30)
-# (9:00-11:00, 13:00-15:00) “minus” (9:00-9:15, 10:00-10:15, 12:30-16:00)= (9:15-10:00, 10:15-11:00)
-
 class TestTimeSubtractor < Minitest::Test
   def setup
     # high_risk_phrases = "Kitten\ncat\n"
@@ -37,5 +31,42 @@ class TestTimeSubtractor < Minitest::Test
     assert_equal 12.5, subtracted[0].end_time
     assert_equal 13.5, subtracted[1].start_time
     assert_equal 14, subtracted[1].end_time
+  end
+
+  def test_remove_overlap_one_in_each_second_included_in_first
+    new_range = TimeSubtractor.remove_overlap([TimeRange.new(9, 10)], [TimeRange.new(9, 9.5)])
+    assert_equal 1, new_range.count
+    assert_equal 9.5, new_range[0].start_time
+    assert_equal 10, new_range[0].end_time
+  end
+  
+  def test_remove_overlap_one_in_each_second_equal_to_first
+    new_range = TimeSubtractor.remove_overlap([TimeRange.new(9, 10)], [TimeRange.new(9, 10)])
+    assert_equal 0, new_range.count
+  end
+
+  def test_remove_overlap_one_in_each_second_ends_after_first
+    new_range = TimeSubtractor.remove_overlap([TimeRange.new(9, 9.5)], [TimeRange.new(9.5, 15)])
+    assert_equal 1, new_range.count
+    assert_equal 9, new_range[0].start_time
+    assert_equal 9.5, new_range[0].end_time
+  end
+  
+  def test_remove_overlap_two_in_first
+    new_range = TimeSubtractor.remove_overlap([TimeRange.new(9, 9.5), TimeRange.new(10, 10.5)], [TimeRange.new(9.25, 10.25)])
+    assert_equal 2, new_range.count
+    assert_equal 9, new_range[0].start_time
+    assert_equal 9.25, new_range[0].end_time
+    assert_equal 10.25, new_range[1].start_time
+    assert_equal 10.5, new_range[1].end_time
+  end
+
+  def test_remove_overlap_two_in_second
+    new_range = TimeSubtractor.remove_overlap([TimeRange.new(9, 11), TimeRange.new(13, 15)], [TimeRange.new(9, 9.25), TimeRange.new(10, 10.25), TimeRange.new(12.5, 16)])
+    assert_equal 2, new_range.count
+    assert_equal 9.25, new_range[0].start_time
+    assert_equal 10, new_range[0].end_time
+    assert_equal 10.25, new_range[1].start_time
+    assert_equal 11, new_range[1].end_time
   end
 end
--- time_subtractor.rb
+++ time_subtractor.rb
@@ -68,21 +68,5 @@ class TimeSubtractor
   end
 end
 
-# array = remove_overlap([TimeRange.new(9, 10)], [TimeRange.new(9, 9.5)])
-# puts array.inspect
-# puts "false 1- should be 9.5-10" if array.length == 0 || array[0].start_time != 9.5 || array[0].end_time != 10
-# array = remove_overlap([TimeRange.new(9, 10)], [TimeRange.new(9, 10)])
-# puts array.inspect
-# puts "false 2- should be empty" if array.length != 0
-# array = remove_overlap([TimeRange.new(9, 9.5)], [TimeRange.new(9.5, 15)])
-# puts array.inspect
-# puts "false 3- should be 9-9.5" if !array || array[0].start_time != 9 || array[0].end_time != 9.5
-# array = remove_overlap([TimeRange.new(9, 9.5), TimeRange.new(10, 10.5)], [TimeRange.new(9.25, 10.25)])
-# puts array.inspect
-# puts "false 4- should be 9-9.25, 10.25-10.5"  if !array || array[0].start_time != 9 || array[0].end_time != 9.25 || array[1].start_time != 10.25 || array[1].end_time != 10.5
-# array = remove_overlap([TimeRange.new(9, 11), TimeRange.new(13, 15)], [TimeRange.new(9, 9.25), TimeRange.new(10, 10.25), TimeRange.new(12.5, 16)])
-# puts array.inspect
-# puts "false 5- should be 9.25-10, 10.25-11" if !array || array[0].start_time != 9.25 || array[0].end_time != 10 || array[1].start_time != 10.25 || array[1].end_time != 11
-
 # trier les listes
 # enlever les doublons au début
